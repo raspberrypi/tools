39974946800662f974551d1c3e3ad25ee4979a09
popcornmix <popcornmix@gmail.com>
2016-02-15 20:17:38 +0000

    Revert "Add linaro 4.9.3 tools"
    This reverts commit b81697cd73dc7378b4f59a32795e5d54766752f0.

--- arm-bcm2708/gcc-linaro-4.9-2015.02-3-x86_64_arm-linux-gnueabihf/lib/gcc/arm-linux-gnueabihf/4.9.3/plugin/include/tm_p.h
+++
@@ -1,9 +0,0 @@
-#ifndef GCC_TM_P_H
-#define GCC_TM_P_H
-#ifdef IN_GCC
-# include "config/arm/arm-protos.h"
-# include "config/arm/aarch-common-protos.h"
-# include "config/linux-protos.h"
-# include "tm-preds.h"
-#endif
-#endif /* GCC_TM_P_H */
--- arm-bcm2708/gcc-linaro-4.9-2015.02-3-x86_64_arm-linux-gnueabihf/share/dejagnu/stub-loader.c
+++
@@ -1,7 +0,0 @@
-/* Bleah!! */
-int remote_debug = 0;
-
-main() {
-  set_debug_traps();
-  breakpoint();
-}
